6872889484f7525b9ce65598ccc4c0a82391d778
Dawn-Feather <c1003183598@gmail.com>
2022-09-19 16:50:43 -0700

    update simulation report and code style

--- src/hdl/version2-hongjiang/fpga/HSn_balenced.sv
+++ src/hdl/version2-hongjiang/fpga/HSn_balenced.sv
@@ -20,55 +20,63 @@ module HSn_balenced #(parameter N = 7'b0010110, parameter MIN_POS = 0, parameter
 		//when N = 2, instantiate 2 unit_seeker modules
 		//where the range of each unit_seeker is half of the full range
 		if (N == 7'b0000010) begin
-			unit_seeker #(.INIT_POS(MIN_POS), .END_POS(MID_POS), .POS_STEP(1))    seekerL (.rst_i, .clk_i,
-																							 .gbox_buffer, .gbox_cnt, .buffer_dv,
-																							 .is_synced(is_sync_temp[0]),
-																							 .offset_pos(offset_pos_temp[0]));
+			unit_seeker #(.INIT_POS(MIN_POS), .END_POS(MID_POS), .POS_STEP(1))
+							seekerL (.rst_i, .clk_i,
+									 .gbox_buffer, .gbox_cnt, .buffer_dv,
+									 .is_synced(is_sync_temp[0]),
+									 .offset_pos(offset_pos_temp[0]));
 																			 
-			unit_seeker #(.INIT_POS(MID_POS+1), .END_POS(MAX_POS), .POS_STEP(1))  seekerR (.rst_i, .clk_i,
-																						 .gbox_buffer, .gbox_cnt, .buffer_dv,
-																						 .is_synced(is_sync_temp[1]),
-																						 .offset_pos(offset_pos_temp[1]));
+			unit_seeker #(.INIT_POS(MID_POS+1), .END_POS(MAX_POS), .POS_STEP(1))
+							seekerR (.rst_i, .clk_i,
+									 .gbox_buffer, .gbox_cnt, .buffer_dv,
+									 .is_synced(is_sync_temp[1]),
+									 .offset_pos(offset_pos_temp[1]));
 		end
 		//when N = 3, instantiate 1 unit_seeker module and 1 HSn module with N' = N-1 = 2
 		//where the range of unit_seeker is 1 and range of HSn is 2
 		else if (N == 7'b0000011) begin
-			unit_seeker #(.INIT_POS(MIN_POS), .END_POS(MIN_POS), .POS_STEP(1))  seekerL (.rst_i, .clk_i,
-																						 .gbox_buffer, .gbox_cnt, .buffer_dv,
-																						 .is_synced(is_sync_temp[0]),
-																						 .offset_pos(offset_pos_temp[0]));
+			unit_seeker #(.INIT_POS(MIN_POS), .END_POS(MIN_POS), .POS_STEP(1))
+							seekerL (.rst_i, .clk_i,
+									 .gbox_buffer, .gbox_cnt, .buffer_dv,
+									 .is_synced(is_sync_temp[0]),
+									 .offset_pos(offset_pos_temp[0]));
 																			 
-		    HSn_balenced #(.N(7'b0000010), .MIN_POS(MIN_POS+1), .MAX_POS(MAX_POS), .IS_TOP_ALIGNER(1'b0))	seekerR (.rst_i, .clk_i,
-																						 .gbox_buffer, .gbox_cnt, .buffer_dv,
-																						 .is_synced(is_sync_temp[1]),
-																						 .offset_pos(offset_pos_temp[1]));
+		    HSn_balenced #(.N(7'b0000010), .MIN_POS(MIN_POS+1), .MAX_POS(MAX_POS), .IS_TOP_ALIGNER(1'b0))
+							seekerR (.rst_i, .clk_i,
+									 .gbox_buffer, .gbox_cnt, .buffer_dv,
+									 .is_synced(is_sync_temp[1]),
+									 .offset_pos(offset_pos_temp[1]));
 		end
 		//when N is odd, instantiate 2 HSn modules with N' = N / 2 and N'' = (N/2) + 1
 		//where one HSn has range of (MAX_POS-MIN_POS)/2 and another has range of (MAX_POS-MIN_POS)/2 + 1  
 		else if (N[0] == 1'b1) begin
 			//{N[6:1],1'b0}+1
-			HSn_balenced #(.N({1'b0, N[6:1]}+1), .MIN_POS(MIN_POS), .MAX_POS(MID_POS), .IS_TOP_ALIGNER(1'b0))  seekerL (.rst_i, .clk_i,
-																						 .gbox_buffer, .gbox_cnt, .buffer_dv,
-																						 .is_synced(is_sync_temp[0]),
-																						 .offset_pos(offset_pos_temp[0]));
+			HSn_balenced #(.N({1'b0, N[6:1]}+1), .MIN_POS(MIN_POS), .MAX_POS(MID_POS), .IS_TOP_ALIGNER(1'b0))
+							seekerL (.rst_i, .clk_i,
+									 .gbox_buffer, .gbox_cnt, .buffer_dv,
+									 .is_synced(is_sync_temp[0]),
+									 .offset_pos(offset_pos_temp[0]));
 																			 
-		    HSn_balenced #(.N({1'b0, N[6:1]}), .MIN_POS(MID_POS+1), .MAX_POS(MAX_POS), .IS_TOP_ALIGNER(1'b0))	seekerR (.rst_i, .clk_i,
-																						 .gbox_buffer, .gbox_cnt, .buffer_dv,
-																						 .is_synced(is_sync_temp[1]),
-																						 .offset_pos(offset_pos_temp[1]));
+		    HSn_balenced #(.N({1'b0, N[6:1]}), .MIN_POS(MID_POS+1), .MAX_POS(MAX_POS), .IS_TOP_ALIGNER(1'b0))
+							seekerR (.rst_i, .clk_i,
+									 .gbox_buffer, .gbox_cnt, .buffer_dv,
+									 .is_synced(is_sync_temp[1]),
+									 .offset_pos(offset_pos_temp[1]));
 		end
 		//when N is even, instantiate 2 HSn modules with N' = N / 2
 		//where each HSn has half of the full range
 		else begin
-			HSn_balenced #(.N({1'b0, N[6:1]}), .MIN_POS(MIN_POS), .MAX_POS(MID_POS), .IS_TOP_ALIGNER(1'b0))   seekerL (.rst_i, .clk_i,
-																						 .gbox_buffer, .gbox_cnt, .buffer_dv,
-																						 .is_synced(is_sync_temp[0]),
-																						 .offset_pos(offset_pos_temp[0]));
+			HSn_balenced #(.N({1'b0, N[6:1]}), .MIN_POS(MIN_POS), .MAX_POS(MID_POS), .IS_TOP_ALIGNER(1'b0))
+							seekerL (.rst_i, .clk_i,
+									 .gbox_buffer, .gbox_cnt, .buffer_dv,
+									 .is_synced(is_sync_temp[0]),
+									 .offset_pos(offset_pos_temp[0]));
 																			 
-			HSn_balenced #(.N({1'b0, N[6:1]}), .MIN_POS(MID_POS+1), .MAX_POS(MAX_POS), .IS_TOP_ALIGNER(1'b0))     seekerR (.rst_i, .clk_i,
-																						 .gbox_buffer, .gbox_cnt, .buffer_dv,
-																						 .is_synced(is_sync_temp[1]),
-																						 .offset_pos(offset_pos_temp[1]));
+			HSn_balenced #(.N({1'b0, N[6:1]}), .MIN_POS(MID_POS+1), .MAX_POS(MAX_POS), .IS_TOP_ALIGNER(1'b0))
+							seekerR (.rst_i, .clk_i,
+									 .gbox_buffer, .gbox_cnt, .buffer_dv,
+									 .is_synced(is_sync_temp[1]),
+									 .offset_pos(offset_pos_temp[1]));
 		end
 
 	
